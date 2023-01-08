b5dfb5c03853f95305541a22d3814bdb2f23a381
gwdean <gwdean@gmail.com>
2023-01-08 08:47:12 -0500

    Added "tea-and-scones.r"

---
+++ tea-and-scones.r
@@ -0,0 +1,9 @@
+## Practice Script
+
+### Tea and Scones example from Pearl-2018
+
+customer <- c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12)
+
+tea <- c(1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 1)
+
+scones <- c(1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1)
